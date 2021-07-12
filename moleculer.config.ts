489eef245b47f8d79ccc6c50a19650eea9303785
moonrailgun <moonrailgun@gmail.com>
2021-07-12 11:41:49 +0800

    chore: 调整输出日志格式

--- moleculer.config.ts
+++ moleculer.config.ts
@@ -7,6 +7,8 @@ import type {
   ServiceBroker,
 } from 'moleculer';
 import type { UserJWTPayload } from './services/types';
+import moment from 'moment';
+import kleur from 'kleur';
 
 /**
  * Moleculer ServiceBroker configuration file
@@ -52,7 +54,14 @@ const brokerConfig: BrokerOptions = {
         // Print module names with different colors (like docker-compose for containers)
         moduleColors: false,
         // Line formatter. It can be "json", "short", "simple", "full", a `Function` or a template string like "{timestamp} {level} {nodeID}/{mod}: {msg}"
-        formatter: 'full',
+        // formatter: 'full',
+        formatter(type, args, bindings, { printArgs }) {
+          return [
+            kleur.grey(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`),
+            `${this.levelColorStr[type]}`,
+            ...printArgs(args),
+          ];
+        },
         // Custom object printer. If not defined, it uses the `util.inspect` method.
         objectPrinter: null,
         // Auto-padding the module name in order to messages begin at the same column.
