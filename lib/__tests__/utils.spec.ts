4e0aea7812266493729db8efad23a13d11ed7aaf
moonrailgun <moonrailgun@gmail.com>
2021-11-25 11:24:49 +0800

    test: add some testcase

--- lib/__tests__/utils.spec.ts
+++ lib/__tests__/utils.spec.ts
@@ -1,4 +1,9 @@
-import { generateRandomStr, getEmailAddress } from '../utils';
+import {
+  generateRandomStr,
+  getEmailAddress,
+  isValidStr,
+  sleep,
+} from '../utils';
 
 describe('getEmailAddress', () => {
   test.each([
@@ -18,4 +23,29 @@ describe('generateRandomStr', () => {
   test('should generate string with manual length', () => {
     expect(generateRandomStr(4)).toHaveLength(4);
   });
+});
+
+describe('isValidStr', () => {
+  test.each<[any, boolean]>([
+    [false, false],
+    [true, false],
+    [0, false],
+    [1, false],
+    ['', false],
+    [{}, false],
+    [[], false],
+    ['foo', true],
+  ])('%p is %p', (input, output) => {
+    expect(isValidStr(input)).toBe(output);
+  });
+});
+
+test('sleep', async () => {
+  const start = new Date().valueOf();
+  await sleep(1000);
+  const end = new Date().valueOf();
+
+  const duration = end - start;
+  expect(duration).toBeGreaterThanOrEqual(1000);
+  expect(duration).toBeLessThan(1050);
 });
