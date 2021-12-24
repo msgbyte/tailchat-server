1aa92728c8da0982e04f2aa69636413dfbe85e05
moonrailgun <moonrailgun@gmail.com>
2021-12-24 14:49:43 +0800

    refactor: task 插件基本前端页面

--- plugins/com.msgbyte.tasks/web/plugins/com.msgbyte.tasks/src/index.tsx
+++ plugins/com.msgbyte.tasks/web/plugins/com.msgbyte.tasks/src/index.tsx
@@ -1,1 +1,9 @@
-console.log('Tasks plugins has been load!');
+import { regCustomPanel, localTrans, Loadable } from '@capital/common';
+
+regCustomPanel({
+  position: 'personal',
+  icon: 'mdi:checkbox-marked-outline',
+  name: 'com.msgbyte.tasks/tasksPanel',
+  label: localTrans({ 'zh-CN': '任务', 'en-US': 'Tasks' }),
+  render: Loadable(() => import('./TasksPanel')),
+});
